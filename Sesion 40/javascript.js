e1428ad9dfe0e8b951790ca3db363d24ff61660d
JacquelinePaz4456789 <adrianvillena703@gmail.com>
2024-08-29 14:12:48 -0500

    first commit

---
+++ Sesion 40/javascript.js
@@ -0,0 +1,33 @@
+console.log ("hola chicas")
+//const area = 25;
+//area=2;
+// let lista = [];
+console.log (lista);
+
+lista[0]=2;
+lista[1]=5;
+lista[2]=7
+lista[3]=11;
+
+
+// console.log(lista);
+
+// console.log(lista[2]);
+// console.log(lista[0]);
+// console.log(lista[3]);
+
+// lista[3]=15;
+// console.log(lista);
+// console.log(lista[3]);
+
+// let contador=0;
+// //el let es para que cuente las vueltas :D en este caso
+// //las vueltas empezaran en 0
+// while (contador < lista.length) {
+//     console.log(lista[contador]);
+//     contador = contador + 1;
+// }
+
+for (let index=0; index < lista.length; index++){
+    console.log(lista[index]);
+}
